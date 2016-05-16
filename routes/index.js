3f9945d3e6daf5bd0f857489bb212b56056aac89
rodmcnew <rodmcnew@gmail.com>
2016-05-16 11:07:29 -0500

    make easier to understand

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
 var rcm = require('../controllers/rcm');
 
 var mysql = require('mysql');
@@ -13,8 +10,19 @@ var conn = mysql.createConnection({
 
 conn.connect();
 
+var rcmInstance = rcm(conn);
+
+
+
+////////////////////////
+
+
+
+
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.get('/:pageName?', rcm(conn));
+router.get('/:pageName?', rcmInstance);
 
 module.exports = router;
