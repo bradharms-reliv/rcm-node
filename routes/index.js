4ea7640caa6ca8c91cce806a97e198f8bde1db8c
rodmcnew <rodmcnew@gmail.com>
2016-05-17 14:41:35 -0500

    organize folders

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,21 @@
 var express = require('express');
-var rcmIsAllowed = require('../acl/rcmIsAllowed');
-var sharedSessionReader = require('../middleware/sharedSessionReader');
+var isAllowed = require('../rcmAdaptor/acl/isAllowed');
+var sharedSessionReader = require('../rcmAdaptor/middleware/sharedSessionReader');
+var pageController = require('../controllers/rcm');
 
 
 var router = express.Router();
 
-router.use(sharedSessionReader);
+router.use(sharedSessionReader); //Injects sessions from RcmUser into req.session
 
 router.get('/', function (req, res, next) {//@TODO remove this acl test
-    rcmIsAllowed(req, 'id-image', null, function (isAllowed, reason) {
+    isAllowed(req, 'id-image', null, function (isAllowed, reason) {
         console.log('isAllowed:', isAllowed, reason);
     });
     next();
 });
 
-router.get('/:pageName?', require('../controllers/rcm'));
+router.get('/:pageName?', pageController); //Renders rcm pages
 
 
 module.exports = router;
