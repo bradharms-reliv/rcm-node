9b68fde9599292a156f010907d0cdaa4103e009c
rodmcnew <rodmcnew@gmail.com>
2016-05-17 12:30:39 -0500

    add rcmIsAllowed module rough draft.

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var rcmIsAllowed = require('../acl/rcmIsAllowed');
 
 router.use(require('../middleware/sharedSessionReader'));
 
-router.get('/', function (req, res, next) {//@TODO remove this acl test
-    rcmIsAllowed(req, 'id-image', null, function (isAllowed, reason) {
-        console.log('isAllowed:', isAllowed, reason);
-    });
-    next();
-});
+//router.get('/', function (req, res, next) {//@TODO remove this acl test
+//    rcmIsAllowed(req, 'id-image', null, function (isAllowed, reason) {
+//        console.log('isAllowed:', isAllowed, reason);
+//    });
+//    next();
+//});
 
 router.get('/:pageName?', require('../controllers/rcm'));
 
