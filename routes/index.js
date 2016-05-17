14bbe600dbcb15ffda9e28f0854fe20eaecdcc19
rodmcnew <rodmcnew@gmail.com>
2016-05-17 12:57:57 -0500

    add rcmIsAllowed module rough draft.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
-var router = express.Router();
 var rcmIsAllowed = require('../acl/rcmIsAllowed');
+var sharedSessionReader = require('../middleware/sharedSessionReader');
 
-router.use(require('../middleware/sharedSessionReader'));
+
+var router = express.Router();
+
+router.use(sharedSessionReader);
 
 router.get('/', function (req, res, next) {//@TODO remove this acl test
     rcmIsAllowed(req, 'id-image', null, function (isAllowed, reason) {
